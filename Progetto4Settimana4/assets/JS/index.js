4533aeba1acfe620321f023f7ba2bb7d6b202d67
ivan1998iasnig@gmail.com <ivan1998iasnig@gmail.com>
2023-04-28 20:38:20 +0200

    moddd

--- Progetto4Settimana4/assets/JS/index.js
+++ Progetto4Settimana4/assets/JS/index.js
@@ -1,17 +1,23 @@
 const pieces = document.querySelectorAll('g[opacity="0"]');
-let opacityOneCount = 0;
+let opacityCount = 0;
 
-setInterval(toggleOpacity, 50);
+setInterval(opacity, 70);
 
-function toggleOpacity() {
+function opacity() {
   const arrayElOpacita0 = Array.from(pieces);
   const x = Math.floor(Math.random() * arrayElOpacita0.length); // ci dovrebbero essere 276 elementi con opacità 0, quindi mettere arrayElOpacita0.length oppure 277 dovrebbe essere la stessa cosa
-  const randomPiece = arrayElOpacita0[x];
-  randomPiece.setAttribute("opacity", "1");
-  opacityOneCount++;
-  if (opacityOneCount === 100) {
-    opacityOneCount = 0;
-    pieces.forEach((M) => M.setAttribute("opacity", "0"));
+  const random = arrayElOpacita0[x];
+  random.setAttribute("opacity", "1");
+  opacityCount++;
+  if (opacityCount === arrayElOpacita0.length) {
+    const x = Math.floor(Math.random() * arrayElOpacita0.length);
+    const random = arrayElOpacita0[x];
+    random.setAttribute("opacity", "0");
+    opacityCount--;
+    if (opacityCount === 0) {
+      clearInterval();
+      opacity();
+    }
   }
 }
 
@@ -19,7 +25,7 @@ window.addEventListener("scroll", function () {
   var nav = document.querySelector("#abc");
   var bottone = document.querySelector(".btn");
   var scrollPosition = window.scrollY;
-  if (scrollPosition > 200) {
+  if (scrollPosition > 280) {
     nav.style.backgroundColor = "white";
     bottone.style.backgroundColor = "rgb(2, 71, 0)";
   } else {
