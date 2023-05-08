08b7e1333f8917f53fffae664c2701bc4288f245
ivan1998iasnig@gmail.com <ivan1998iasnig@gmail.com>
2023-05-08 17:52:02 +0200

    Risolto problema con undefined

--- JSIII/EsercigioLunedì/index.js
+++ JSIII/EsercigioLunedì/index.js
@@ -5,11 +5,14 @@ class User {
     this.age = age;
     this.location = location;
   }
+
   confrontaEta(a) {
     if (this.age === a.age) {
-      return console.log(`${this.firstName} e ${a.firstName} hanno la stessa età.`);
+      return console.log(
+        `${this.firstName} e ${a.firstName} hanno la stessa età.`
+      );
     } else if (this.age > a.age) {
-     return console.log(`${this.firstName} è più anziano di ${a.firstName}.`);
+      return console.log(`${this.firstName} è più anziano di ${a.firstName}.`);
     } else {
       return console.log(`${this.firstName} è più giovane di ${a.firstName}.`);
     }
@@ -20,4 +23,4 @@ const user1 = new User("Ivan", "Iasnig", 24, "Trieste");
 
 const user2 = new User("Pippo", "Baudo", 84, "Milano");
 
-console.log(user1.confrontaEta(user2));
+user1.confrontaEta(user2);
