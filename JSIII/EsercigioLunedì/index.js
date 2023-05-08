7ce2e53f90f3798014e8aa61ff057ce0a501d92e
ivan1998iasnig@gmail.com <ivan1998iasnig@gmail.com>
2023-05-08 17:46:31 +0200

    ciao

--- JSIII/EsercigioLunedì/index.js
+++ JSIII/EsercigioLunedì/index.js
@@ -5,26 +5,19 @@ class User {
     this.age = age;
     this.location = location;
   }
-  confrontaEta(user2) {
-    if (this.age === user2.age) {
-      console.log(
-        `${this.firstName} e ${user2.firstName} hanno la stessa età.`
-      );
-    } else if (this.age > user2.age) {
-      console.log(
-        `${this.firstName} è più anziano di ${user2.firstName}.`
-      );
+  confrontaEta(a) {
+    if (this.age === a.age) {
+      return console.log(`${this.firstName} e ${a.firstName} hanno la stessa età.`);
+    } else if (this.age > a.age) {
+     return console.log(`${this.firstName} è più anziano di ${a.firstName}.`);
     } else {
-      console.log(
-        `${this.firstName} è più giovane di ${user2.firstName}.`
-      );
+      return console.log(`${this.firstName} è più giovane di ${a.firstName}.`);
     }
   }
 }
 
-const user1 = new User("Giovanni", "Bianchi", 28, "Roma");
+const user1 = new User("Ivan", "Iasnig", 24, "Trieste");
 
-const user2 = new User("Maria", "Verdi", 34, "Milano");
+const user2 = new User("Pippo", "Baudo", 84, "Milano");
 
 console.log(user1.confrontaEta(user2));
-
