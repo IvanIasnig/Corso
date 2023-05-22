e414030f3fc0b5a556c26bb8a4a2b3b189e042f5
ivan1998iasnig@gmail.com <ivan1998iasnig@gmail.com>
2023-05-22 14:52:59 +0200

    ciao

--- Modulo_Angular/Settimana_I/giocatori.js
+++ Modulo_Angular/Settimana_I/giocatori.js
@@ -5,22 +5,22 @@ function confrontazione(player1, player2) {
     var x = document.getElementById("ciao");
     console.log(num_cas);
     if (player1 === num_cas) {
-        return x.innerHTML = "Il giocatore che ha vinto \u00E8 il player 1";
+        return x.innerHTML = "Il giocatore che ha vinto \u00E8 il player <h3>1</h3>";
     }
     else if (player2 === num_cas) {
-        return x.innerHTML = "Il giocatore che ha vinto \u00E8 il player 2";
+        return x.innerHTML = "Il giocatore che ha vinto \u00E8 il player <h3>2</h3>";
     }
     else {
         var player_1_avv = Math.abs(num_cas - player1);
         var player_2_avv = Math.abs(num_cas - player2);
         if (player_1_avv < player_2_avv) {
-            return x.innerHTML = "Il giocatore 1 non ha vinto, ma si \u00E8 avvicinato di pi\u00F9";
+            return x.innerHTML = "Il giocatore <h3>1</h3> non ha vinto, ma si \u00E8 avvicinato di pi\u00F9";
         }
         else if (player_1_avv === player_2_avv) {
             return x.innerHTML = "pareggio!";
         }
         else {
-            return x.innerHTML = "Il giocatore 2 non ha vinto, ma si \u00E8 avvicinato di pi\u00F9";
+            return x.innerHTML = "Il giocatore <h3>2</h3> non ha vinto, ma si \u00E8 avvicinato di pi\u00F9";
         }
     }
 }
--- Modulo_Angular/Settimana_I/giocatori.ts
+++ Modulo_Angular/Settimana_I/giocatori.ts
@@ -8,18 +8,18 @@ function confrontazione(player1: number, player2: number) {
     console.log(num_cas);
     if (player1 === num_cas) {
       
-      return  x.innerHTML = `Il giocatore che ha vinto è il player 1`;
+      return  x.innerHTML = `Il giocatore che ha vinto è il player <h3>1</h3>`;
     } else if (player2 === num_cas) {
-      return  x.innerHTML = `Il giocatore che ha vinto è il player 2`;
+      return  x.innerHTML = `Il giocatore che ha vinto è il player <h3>2</h3>`;
     } else {
       let player_1_avv: number = Math.abs(num_cas - player1);
       let player_2_avv: number = Math.abs(num_cas - player2);
       if (player_1_avv < player_2_avv) {
-        return  x.innerHTML = `Il giocatore 1 non ha vinto, ma si è avvicinato di più`;
+        return  x.innerHTML = `Il giocatore <h3>1</h3> non ha vinto, ma si è avvicinato di più`;
       } else if (player_1_avv === player_2_avv) {
         return  x.innerHTML = `pareggio!`;
       } else {
-        return  x.innerHTML = `Il giocatore 2 non ha vinto, ma si è avvicinato di più`;
+        return  x.innerHTML = `Il giocatore <h3>2</h3> non ha vinto, ma si è avvicinato di più`;
       }
     }
   }
